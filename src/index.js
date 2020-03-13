8aecbb10d756d43b3d20463e59b8589c8591308b
m.narminov <m.narminov@gmail.com>
2020-03-14 00:35:49 +0300

    fix: try to solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+  let wordsCount = expr.length / 10;
+  let arr = []; 
+  let res = '';
+
+  for (let k = 0; k < wordsCount; k++) {
+    let nextWord = ''
+    for (let i = k * 10; i < expr.length; i++) {
+      if (k % 9 !== 0) {
+        nextWord += expr[i];
+      }
+    }
+    arr.push(nextWord);
+  }
+
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === '**********') {
+      res += ' ';
+    } else {
+
+      let word = +arr[i];
+      let key = '';
+
+      while (word % 100 !== 0) {
+        let wordStr = word / String(word).length - 2;
+        let symb = wordStr === 10 ? '.' : '-'
+        key += symb;
+        word = word / 100 
+      }
+      
+      res += MORSE_TABLE[key];
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
