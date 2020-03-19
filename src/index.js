12d61e5fd7a08e04fac35d0a68ad61245d4e5aef
m.narminov <m.narminov@gmail.com>
2020-03-19 16:13:36 +0300

    fix: potential vulnerability in minimist

--- src/index.js
+++ src/index.js
@@ -1,73 +1,67 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0'
 };
 
 function decode(expr) {
-
-  let wordsCount = expr.length / 10;
-  let arr = []; 
+  let arr = [];
   let res = '';
 
-  for (let k = 0; k < wordsCount; k++) {
-    let nextWord = ''
-    for (let i = k * 10; i < expr.length; i++) {
-      if (k % 9 !== 0) {
-        nextWord += expr[i];
-      }
-    }
+  for (let k = 0; k < expr.length; k += 10) {
+    let nextWord = expr.slice(k, k + 10);
     arr.push(nextWord);
   }
 
-  for (let i = 0; i < arr.length; i++){
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '**********') {
       res += ' ';
     } else {
-
       let word = +arr[i];
       let key = '';
 
       while (word % 100 !== 0) {
-        let wordStr = word / String(word).length - 2;
-        let symb = wordStr === 10 ? '.' : '-'
+        let wordStr = word / (String(word).length - 2);
+        let symb = +wordStr === 10 ? '.' : '-';
         key += symb;
-        word = word / 100 
+        word = word / 100;
       }
-      
+
       res += MORSE_TABLE[key];
     }
   }
@@ -76,5 +70,5 @@ function decode(expr) {
 }
 
 module.exports = {
-    decode
-}
+  decode
+};
